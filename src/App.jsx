f654b8b90cd93cf160235fbf2c82aac8cebc05ee
ahmedhossam232 <wrwamnwat21@gmail.com>
2023-07-18 03:05:07 +0300

    upload

--- src/App.jsx
+++ src/App.jsx
@@ -2,19 +2,19 @@ import React from 'react'
 import './style/App.css'
 import './style/Media.css'
 import Home from './components/home/Home';
-import { BrowserRouter,Route,Routes} from 'react-router-dom';
+import { HashRouter,Route,Routes} from 'react-router-dom';
 import Plu from './components/+';
 import _ from './components/-';
 function App() {
 
   return (
-   <BrowserRouter basename='/Learn-Math-For-Kids'>
+   <HashRouter basename='/Learn-Math-For-Kids'>
 <Routes> 
   <Route expect path='/Learn-Math-For-Kids' element={<Home />} />
   <Route path='/Plu' element={<Plu />} />
   <Route path='/M' element={<_ />} />
 </Routes>
-     </BrowserRouter>
+     </HashRouter>
   )
 }
 
